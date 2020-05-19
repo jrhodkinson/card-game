cba9f9c3f46156d95a4d3f4992178147f6d292f3
Jack Hodkinson <jrh206@cam.ac.uk>
2020-05-19 22:07:58 +0100

    Create StickyBehaviour base

---
+++ card-game-application/src/main/java/jrh/game/card/behaviour/StickyBehaviour.java
@@ -0,0 +1,13 @@
+package jrh.game.card.behaviour;
+
+import jrh.game.card.Card;
+import jrh.game.match.Match;
+import jrh.game.match.Player;
+
+public class StickyBehaviour implements Behaviour {
+
+    @Override
+    public void play(Match match, Player player, Card card) {
+
+    }
+}
