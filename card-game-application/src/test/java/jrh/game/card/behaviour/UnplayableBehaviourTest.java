7d619181ecb35c9ff3f63f810b8744c7a1d711fd
Jack Hodkinson <jrh206@cam.ac.uk>
2020-05-21 22:33:53 +0100

    Add UnplayableBehaviour round trip test

--- card-game-application/src/test/java/jrh/game/card/behaviour/UnplayableBehaviourTest.java
+++ card-game-application/src/test/java/jrh/game/card/behaviour/UnplayableBehaviourTest.java
@@ -1,4 +1,11 @@
 package jrh.game.card.behaviour;
 
+import org.junit.jupiter.api.Test;
+
 public class UnplayableBehaviourTest {
+
+    @Test
+    public void roundTripsViaJson() {
+        TestBehaviour.roundTripsViaJson(new UnplayableBehaviour());
+    }
 }
