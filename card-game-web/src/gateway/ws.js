94391cfd7d7dbc9d7eff6227a17da344ea2a2d06
Jack Hodkinson <jrh206@cam.ac.uk>
2020-06-18 22:30:35 +0100

    Send pong back as response to ping

--- card-game-web/src/gateway/ws.js
+++ card-game-web/src/gateway/ws.js
@@ -2,6 +2,9 @@ import ReconnectingWebSocket from "reconnecting-websocket";
 import { receivedMatchState } from "../store/match-actions";
 import { receivedPing } from "../store/socket-actions";
 
+const PING = "ping";
+const PONG = "pong";
+
 const MESSAGE_ACTION_CREATORS = {
   ping: (payload) => receivedPing(payload),
   matchState: (payload) => receivedMatchState(payload),
@@ -19,14 +22,19 @@ const toAction = (message) => {
   return MESSAGE_ACTION_CREATORS[message.type](message.payload);
 };
 
+const handleMessage = (dispatch, ws) => (event) => {
+  const message = JSON.parse(event.data);
+  if (message.type === PING) {
+    ws.send(JSON.stringify({ type: PONG, payload: message.payload }));
+  }
+  const action = toAction(message);
+  if (action) {
+    dispatch(action);
+  }
+};
+
 export const connectToMatchWebSocket = (dispatch) => {
   const ws = new ReconnectingWebSocket("ws://localhost:7000/match");
-  ws.onmessage = (event) => {
-    const message = JSON.parse(event.data);
-    const action = toAction(message);
-    if (action) {
-      dispatch(action);
-    }
-  };
+  ws.onmessage = handleMessage(dispatch, ws);
   return ws.close;
 };
