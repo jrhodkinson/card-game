d208ba1266eaea462ac2419159679f6512782e37
Jack Hodkinson <jrh206@cam.ac.uk>
2020-07-08 21:15:57 +0100

    Introduce SubscribeAll annotation

---
+++ card-game-events/src/main/java/jrh/game/api/SubscribeAll.java
@@ -0,0 +1,13 @@
+package jrh.game.api;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Retention(RUNTIME)
+@Target(METHOD)
+public @interface SubscribeAll {
+    Class<? extends Event>[] events();
+}
