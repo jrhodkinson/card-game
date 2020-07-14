0aee512013b22c19de266b403c51a1621c713185
Jack Hodkinson <jrh206@cam.ac.uk>
2020-07-14 21:48:54 +0100

    Make TauntPower block incoming attacks

--- card-game-api/src/main/java/jrh/game/api/Damageable.java
+++ card-game-api/src/main/java/jrh/game/api/Damageable.java
@@ -1,5 +1,9 @@
 package jrh.game.api;
 
+import jrh.game.common.EntityId;
+
 public interface Damageable {
+
+    EntityId getEntityId();
 
 }
--- card-game-application/src/main/java/jrh/game/match/HealthController.java
+++ card-game-application/src/main/java/jrh/game/match/HealthController.java
@@ -3,10 +3,18 @@ package jrh.game.match;
 import jrh.game.api.Controller;
 import jrh.game.api.Damageable;
 import jrh.game.api.Player;
+import jrh.game.api.Structure;
+import jrh.game.common.EntityId;
 import jrh.game.structure.MutableStructure;
 import jrh.game.structure.StructureHealthController;
+import jrh.game.structure.StructureStateController;
+import jrh.game.structure.power.TauntPower;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
 
 public class HealthController implements Controller {
 
@@ -23,6 +31,10 @@ public class HealthController implements Controller {
     }
 
     public void damage(Player source, Damageable damageable, int amount) {
+        if (!canDamage(source, damageable)) {
+            logger.info("Player={} is currently unable to damage target={} (does something else have taunt?)", source, damageable);
+            return;
+        }
         int finalAmount = match.getModificationComputer().computeModifiedDamage(source, damageable, amount);
         if (damageable instanceof Player) {
             playerHealthController.damage((Player) damageable, finalAmount);
@@ -41,5 +53,26 @@ public class HealthController implements Controller {
         } else {
             logger.error("Unknown damageable={} of type={}", damageable, damageable.getClass());
         }
+    }
+
+    private boolean canDamage(Player source, Damageable target) {
+        Player otherPlayer = match.getOtherPlayer(source.getUser());
+        StructureStateController structureStateController = match.getController(StructureStateController.class);
+        List<EntityId> otherPlayersStructuresWithTaunt = structuresWithTaunt()
+            .stream()
+            .filter(s -> structureStateController.getOwner(s).equals(otherPlayer))
+            .map(Structure::getEntityId)
+            .collect(toList());
+        if (otherPlayersStructuresWithTaunt.isEmpty()) {
+            return true;
+        }
+        return otherPlayersStructuresWithTaunt.contains(target.getEntityId());
+    }
+
+    private List<Structure> structuresWithTaunt() {
+        return match.getAllStructures()
+            .stream()
+            .filter(s -> s.hasPower(TauntPower.class))
+            .collect(toList());
     }
 }
