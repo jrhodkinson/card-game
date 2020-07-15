8d762c32f2136d12fde466683ddc55ba485b73db
Jack Hodkinson <jrh206@cam.ac.uk>
2020-07-15 22:10:27 +0100

    Add sctructure descriptions to dto

--- card-game-service/src/main/java/jrh/game/service/websocket/server/dto/StructureDto.java
+++ card-game-service/src/main/java/jrh/game/service/websocket/server/dto/StructureDto.java
@@ -11,17 +11,19 @@ public class StructureDto {
     public final String name;
     public final String flavor;
     public final int health;
+    public final String description;
 
-    private StructureDto(EntityId entityId, StructureId structureId, String name, String flavor, int health) {
+    private StructureDto(EntityId entityId, StructureId structureId, String name, String flavor, int health, String description) {
         this.entityId = entityId;
         this.structureId = structureId;
         this.name = name;
         this.flavor = flavor;
         this.health = health;
+        this.description = description;
     }
 
     public static StructureDto fromStructure(Structure structure) {
         return new StructureDto(structure.getEntityId(), structure.getStructureId(), structure.getName(),
-                structure.getFlavorText().orElse(null), structure.getHealth());
+                structure.getFlavorText().orElse(null), structure.getHealth(), structure.getDescription().toString());
     }
 }
