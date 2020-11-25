884a47b747846d96a92621fe4cb878c5672225f4
Jack Hodkinson <jrh206@cam.ac.uk>
2020-11-25 22:14:04 +0000

    Store Sessions in Cache

--- card-game-service/src/main/java/jrh/game/service/account/Sessions.java
+++ card-game-service/src/main/java/jrh/game/service/account/Sessions.java
@@ -1,23 +1,33 @@
 package jrh.game.service.account;
 
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
 import jrh.game.common.account.AccountId;
 
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
 
 public class Sessions {
 
-    private final Map<AccountId, Token> tokens = new ConcurrentHashMap<>();
+    private final LoadingCache<AccountId, Token> tokens = CacheBuilder.newBuilder()
+        .expireAfterAccess(3, TimeUnit.DAYS)
+        .build(new CacheLoader<>() {
+            @Override
+            public Token load(AccountId accountId) {
+                return Token.randomToken();
+            }
+        });
 
     public Token getToken(AccountId accountId) {
-        return tokens.computeIfAbsent(accountId, (id) -> Token.randomToken());
+        return tokens.getUnchecked(accountId);
     }
 
     public boolean isValid(AccountId accountId, Token token) {
-        Token expectedToken = tokens.get(accountId);
+        Token expectedToken = tokens.getIfPresent(accountId);
         if (token == null) {
             return false;
         }
+
         return token.equals(expectedToken);
     }
 }
