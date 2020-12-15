a3a5f1e6ccf8e289c5bf3f24a17f0c77a89d64cf
Jack Hodkinson <jrh206@cam.ac.uk>
2020-12-15 22:58:25 +0000

    Reduce weight of unimportant features

--- card-game-web/src/components/styles/colors.js
+++ card-game-web/src/components/styles/colors.js
@@ -5,6 +5,7 @@ export const darkBlack = "#181F25";
 export const darkGrey = "#202931";
 export const mediumGrey = "#415363";
 export const lightGrey = "#51677B";
+export const veryLightGrey = "#8898A6";
 export const lightestGrey = "#C4CDD4";
 export const offWhite = "#DAE1E7";
 
@@ -13,7 +14,7 @@ export const red = "#D81B60";
 export const accentRed = "#C51162";
 
 export const textOnBlack = white;
-export const faintTextOnBlack = lightestGrey;
+export const faintTextOnBlack = veryLightGrey;
 
 export const textOnWhite = darkGrey;
 export const faintTextOnWhite = mediumGrey;
