9b33c47c0f189313bfa361babcb773abe1997aaf
Jack Hodkinson <jrh206@cam.ac.uk>
2020-12-31 20:57:39 +0000

    Implement EndTurnBehaviour

---
+++ card-game-implementation/src/main/java/jrh/game/card/behaviour/EndTurnBehaviour.java
@@ -0,0 +1,41 @@
+package jrh.game.card.behaviour;
+
+import jrh.game.api.Callback;
+import jrh.game.api.Match;
+import jrh.game.api.Subscribe;
+import jrh.game.api.event.CardPlayed;
+import jrh.game.asset.JsonKey;
+import jrh.game.common.description.AtomicDescription;
+import jrh.game.match.MatchStateController;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import static jrh.game.card.behaviour.AbstractBehaviour.TargetType.NO_TARGET;
+
+@JsonKey("endTurn")
+public class EndTurnBehaviour extends AbstractBehaviour {
+
+    private static final Logger logger = LogManager.getLogger(ConstructBehaviour.class);
+
+    public EndTurnBehaviour() {
+        super(NO_TARGET);
+    }
+
+    @Override
+    public AtomicDescription getDescription() {
+        return AtomicDescription.builder().plainString("End your turn").build();
+    }
+
+    @Subscribe
+    private void cardPlayed(CardPlayed cardPlayed, Match match, Callback callback) {
+        if (cardPlayed.getCard().equals(this.getCard())) {
+            logger.info("Ending the current turn");
+            match.getController(MatchStateController.class).endTurn(callback::enqueue);
+        }
+    }
+
+    @Override
+    public EndTurnBehaviour duplicate() {
+        return new EndTurnBehaviour();
+    }
+}
