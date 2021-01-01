7c057efa960d7f876709dac7f8b431e27a6914dc
Jack Hodkinson <jrh206@cam.ac.uk>
2021-01-01 16:56:05 +0000

    Format

--- card-game-implementation/src/main/java/jrh/game/card/CardImplFactory.java
+++ card-game-implementation/src/main/java/jrh/game/card/CardImplFactory.java
@@ -62,11 +62,10 @@ public class CardImplFactory {
         List<CardId> startingDeck = new ArrayList<>();
         Streams
             .concat(
-                Collections.nCopies(4, new CardId("MONEY:1")).stream(),
-                Collections.nCopies(3, new CardId("DAMAGE:1")).stream(),
-                Collections.nCopies(2, new CardId("DEVOTION")).stream(),
-                Stream.of(new CardId("PURGE"))
-            )
+                    Collections.nCopies(4, new CardId("MONEY:1")).stream(),
+                    Collections.nCopies(3, new CardId("DAMAGE:1")).stream(),
+                    Collections.nCopies(2, new CardId("DEVOTION")).stream(),
+                    Stream.of(new CardId("PURGE")))
             .map(this::create)
             .filter(Optional::isPresent)
             .map(Optional::get)
