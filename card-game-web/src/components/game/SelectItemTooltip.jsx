079c371fe4be8fec69060c988abf57883688d4d2
Jack Hodkinson <jrh206@cam.ac.uk>
2021-01-01 15:03:15 +0000

    Add tooltip for targetting card in hand

--- card-game-web/src/components/game/SelectItemTooltip.jsx
+++ card-game-web/src/components/game/SelectItemTooltip.jsx
@@ -1,18 +1,23 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import {
+  selectDoesPendingCardRequireCardInHandTarget,
   selectDoesPendingCardRequireDamageableTarget,
   selectDoesPendingCardRequireStoreTarget,
 } from "../../store/play/play-selector";
 import MouseTooltip from "../common/MouseTooltip";
 
-const targetMessage = (damageableTarget, storeTarget) => {
+const targetMessage = (damageableTarget, storeTarget, handTarget) => {
   if (damageableTarget) {
     return "Select a player or structure";
   }
 
   if (storeTarget) {
     return "Select a card in the market";
+  }
+
+  if (handTarget) {
+    return "Select a card in your hand";
   }
 
   return "";
@@ -23,9 +28,9 @@ const SelectItemTooltip = () => {
     selectDoesPendingCardRequireDamageableTarget
   );
   const storeTarget = useSelector(selectDoesPendingCardRequireStoreTarget);
-  const anyTarget = damageableTarget || storeTarget;
-  const message = targetMessage(damageableTarget, storeTarget);
-  return <MouseTooltip visible={anyTarget}>{message}</MouseTooltip>;
+  const handTarget = useSelector(selectDoesPendingCardRequireCardInHandTarget);
+  const message = targetMessage(damageableTarget, storeTarget, handTarget);
+  return <MouseTooltip visible={message !== ""}>{message}</MouseTooltip>;
 };
 
 export default SelectItemTooltip;
