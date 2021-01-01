88517211c44c2741e7424c375dec282c763947d1
Jack Hodkinson <jrh206@cam.ac.uk>
2021-01-01 11:52:46 +0000

    Fix builder pattern

--- card-game-common/src/main/java/jrh/game/common/account/Account.java
+++ card-game-common/src/main/java/jrh/game/common/account/Account.java
@@ -42,7 +42,9 @@ public class Account {
 
     @Override
     public String toString() {
-        return new ToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE).append("id", id).append("name", name)
-                .toString();
+        return new ToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE)
+            .append("id", id)
+            .append("name", name)
+            .toString();
     }
 }
